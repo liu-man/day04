2de0d069175249941efd55aed9d76fb387e3a873
liu-man <895996849@qq.com>
2019-06-13 16:24:42 +0800

    first

---
+++ src/components/App.1.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Com extends Component { //继承时不用写react
+  constructor (props) {
+    super(props);
+    this.state = {
+      list: [
+        {
+          brand: '康师傅',
+          data: ['老谭酸菜', '红烧牛肉', '鲜虾鱼板面', '小鸡炖蘑菇']
+        },
+        {
+          brand: '统一',
+          data: ['藤椒牛肉', '葱爆牛肉', '葱烧排骨面']
+        }
+      ]
+    }
+  }
+
+  render () {
+    let arr = [];
+    this.state.list.map((item,index) => {
+      let itemArr = [];
+      item.data.map((itm,idx) => {
+        itemArr.push(<li key = { idx }>{ itm }</li>)
+      })
+      let oli =<li key = { index }>{ item.brand }<ul>{ itemArr }</ul></li>;
+      arr.push(oli)
+    })
+
+    return (
+      <ul>
+        { arr }
+      </ul>
+    )
+  }
+}
+export default Com
